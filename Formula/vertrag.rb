6539021eedb994067151ccf43beeff213300139a
Christopher Thomas <chris.thomas@antimatter-studios.com>
2026-08-18 14:39:13 +0200

    Sync formulae from upstream releases (#102)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/vertrag.rb
+++ Formula/vertrag.rb
@@ -6,28 +6,28 @@
 class Vertrag < Formula
   desc "Contract-test an HTTP API against its OpenAPI description"
   homepage "https://github.com/antimatter-studios/vertrag"
-  version "0.13.1"
+  version "0.13.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/antimatter-studios/vertrag/releases/download/v#{version}/vertrag-#{version}-darwin-arm64.tar.gz"
-      sha256 "09bb2330082b904071d868e23d61dc207f60ab767640a083e8eadd5f08a24d1f"
+      sha256 "1283d17389c568778146a875b3b2fa1764a9473af918a4333a4498e006fe3375"
     end
     on_intel do
       url "https://github.com/antimatter-studios/vertrag/releases/download/v#{version}/vertrag-#{version}-darwin-x86_64.tar.gz"
-      sha256 "2b598798479b68fe54e8f8e33c8b9549de246c79fcb5812e94ee31a43c0a8c9b"
+      sha256 "65d926fd71a92e157b78cd94410e63893b0aedf5b3c63efae803e0616ec2bc7b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/antimatter-studios/vertrag/releases/download/v#{version}/vertrag-#{version}-linux-arm64.tar.gz"
-      sha256 "11dcef5fea708a0072d2e4081a39e6b94fd96084ad4e461167a224c0ea852412"
+      sha256 "24a32f2007f41ec7f4539893830f1963518f3abd995c5b9439f3520b110a9a9c"
     end
     on_intel do
       url "https://github.com/antimatter-studios/vertrag/releases/download/v#{version}/vertrag-#{version}-linux-x86_64.tar.gz"
-      sha256 "45ef8392f0cd1f388c8a0c63aedcb524100ab2bcb543c82d2c3961d4d4b0baab"
+      sha256 "0fd1bf1a1cfd01f672c19153e33b12dae6c29def0e56f947742f826dce521abf"
     end
   end
 
