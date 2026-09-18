7484585d08bef1c354134c417d96fa0903473879
Christopher Thomas <chris.thomas@antimatter-studios.com>
2026-09-18 12:29:45 +0200

    Sync formulae from upstream releases (#182)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/trove-cli.rb
+++ Formula/trove-cli.rb
@@ -4,28 +4,28 @@
 class TroveCli < Formula
   desc "KeePassXC-compatible secrets CLI (trove) + daemon (troved)"
   homepage "https://github.com/antimatter-studios/trove"
-  version "0.17.0"
+  version "0.17.1"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://github.com/antimatter-studios/trove/releases/download/v#{version}/trove-#{version}-darwin-arm64.tar.gz"
-      sha256 "8a17ebd6f4ef0ccd3b339eb6c34a1aa50daa8d39286f4bab92602c95f538526a"
+      sha256 "de7542a1b3e2f105e082af3ad405f83311cafdea785272f32d26f9f8b9607c17"
     end
     on_intel do
       url "https://github.com/antimatter-studios/trove/releases/download/v#{version}/trove-#{version}-darwin-x86_64.tar.gz"
-      sha256 "7c6f2b9f09b7ae74f40490a6c823826fe249c7cfb160f68a9d8fd62c23dd8290"
+      sha256 "47cf0b19ef9193b9815a599bb54b4c784e9673a4b386ca90862967945929d4d2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/antimatter-studios/trove/releases/download/v#{version}/trove-#{version}-linux-arm64.tar.gz"
-      sha256 "da7901db8aa9f10c9c32ff8e3c31b97d199cade5a2152740077c47eeda5a2b20"
+      sha256 "8fbacbcbcb0427f08007f750ec4f2a063d0a22012b3803c93c3bdc0e632a5e37"
     end
     on_intel do
       url "https://github.com/antimatter-studios/trove/releases/download/v#{version}/trove-#{version}-linux-x86_64.tar.gz"
-      sha256 "151ab3040bb672e1eda2003df76ad492dc11dc09cf11af2df58647c2eb712f64"
+      sha256 "14349691c5a81d0acd33bbcc9c9a9ac25e84ffdea1397edc08594364f3ae7977"
     end
   end
 
